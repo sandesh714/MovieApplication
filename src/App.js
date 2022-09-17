01918c4eef359ecd4e0cfa4b2ebb562888e9e94d
sandesh714 <sandeshlamsal714@gmail.com>
2022-09-16 21:57:40 -0500

    Final App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import MovieList from './components/MovieList';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox'; 
 
-function App() {
+
+const App = ()=>  {
+  const [movies, setMovies] = useState([ ]);
+  const [searchValue, setSearchValue] = useState('');
+
+const getMovieRequest = async (searchValue) => {
+  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=cdd6b145`;
+
+  const response = await fetch(url);
+  const responseJson = await response.json();
+  console.log(responseJson);
+  if(responseJson.Search){
+    setMovies(responseJson.Search);
+  }
+};
+
+useEffect(()=>{
+  getMovieRequest(searchValue);
+}, [searchValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies'/>
+        <SearchBox  searchValue = {searchValue} setSearchValue={setSearchValue}/>
+      </div>
+      <div className="row">
+      <MovieList movies = {movies} />
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
