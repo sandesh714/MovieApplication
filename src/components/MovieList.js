17fba88e2614de53febf39a8ff63acfeb3238026
sandesh714 <sandeshlamsal714@gmail.com>
2022-10-03 23:39:18 -0500

    Added AddFavourites and RemoveFavourites.js || Updated Movielist

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 const MovieList = (props) => {
-    return (
-        <>
-        {props.movies.map((movie, index)=> 
-        <div className="d-flex justify-content-start m-3">
-            <img src= {movie.Poster} alt="movie"></img>
-        </div>)}
-        </>
-    )
-}
+    const FavouriteComponent = props.favouriteComponent;
 
+    return (
+
+        <>
+        {props.movies.map((movie, index)=> (
+        <div className="img-container d-flex justify-content-start m-3">
+            <img src= {movie.Poster} alt="movie"></img>
+            <div onClick= {() => props.handleFavouritesClick(movie)} 
+            className='overlay d-flex align-items-center justify-content-center'>
+                <FavouriteComponent/>
+            </div>
+        </div>))}
+        </>
+
+    )
+
+}
 export default MovieList;
